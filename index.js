1a9e59273a0c8c7978ae8688df0c19f9880fe083
Sheryl Chee <sherylmichaela@gmail.com>
2023-12-28 19:58:14 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+let destructivelyAppendCat = () => cats.push("Ralph");
+let destructivelyPrependCat = () => cats.unshift("Bob");
+let destructivelyRemoveLastCat = () => {
+  cats.pop();
+  return cats;
+};
+
+let destructivelyRemoveFirstCat = () => {
+  cats.shift();
+  return cats;
+};
+
+let appendCat = (name) => [...cats, name];
+appendCat("Broom");
+
+let prependCat = (name) => [name, ...cats];
+prependCat("Arnold");
+
+let removeLastCat = () => cats.slice(0, 2);
+removeLastCat();
+
+let removeFirstCat = () => cats.slice(-2);
+removeFirstCat();
